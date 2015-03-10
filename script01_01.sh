029e48f1dcfe33ceeb447397e4062275c51572ee
Dante™ <dante.toscano1997@gmail.com>
2015-03-10 18:53:20 +0100

    Update script01_01.sh

--- script01_01.sh
+++ script01_01.sh
@@ -2,7 +2,7 @@
 
 echo "Il nome del file eseguibile e' \"new\""
 
-if [ $1 -e /c/Documents\ and\ Settings\/Studenti//Desktop/New ]
+if [ $1 -e /c/Documents\ and\ Settings\/Studenti/Desktop/New ]
 then
 	i=0
 	for arg in "$new"
