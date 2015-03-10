f2c383e3670b30e209b099e28d9ce55bb0753d0b
Dante™ <dante.toscano1997@gmail.com>
2015-03-10 18:46:39 +0100

    Update script01_02.sh

--- script01_02.sh
+++ script01_02.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+x="1"
+y="Ciao"
+
+for x in 0 1 2 3
+do
+x=$(( $x+1 ))
+echo $y$x
+done
+
 # Riceve un argomento da riga di comando, composto da un numero intero positivo
 # Scrive "Ciao 1", "Ciao 2", ... uno per riga, da 1 al numero passato come argomento
